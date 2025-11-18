a26c8da855b4573d895be69f5fd6907adda79502
Mike Hyams <mikehyamsjr@gmail.com>
2025-11-18 06:18:11 -0600

    fix: Hardcode deal_tracker schema to fix TypeScript type error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,10 +3,9 @@ import { Database } from './database.types'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const supabaseSchema = process.env.NEXT_PUBLIC_SUPABASE_SCHEMA || 'public'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   db: {
-    schema: supabaseSchema
+    schema: 'deal_tracker'
   }
 })
